17cae5811bb95e3d9b323707042fb41654c5c70c
Matias Aranda <arandamatias849@gmail.com>
2025-05-15 12:21:49 -0300

    Added zoom hover effect to Hero Links images

--- components/Home/Links.tsx
+++ components/Home/Links.tsx
@@ -4,26 +4,47 @@ import Link from 'next/link'
 const Links = () => {
   return (
     <div className='grid grid-cols-2 gap-0'>
-      <div className='h-[700px] flex flex-col items-center justify-end text-center pb-15 bg-[url(/about_bg.png)] bg-cover bg-center bg-no-repeat'>
-        <p className='text-neutral-50'>Sobre Mí</p>
-        <h3 className='text-neutral-50 text-[48px] font-display mx-0 my-4 leading-none'>Andrés Marín</h3>
-        <Link href="/porsche_cup" className='text-primary font-body-light-italic flex gap-2 items-center'>LEER MÁS <img className='h-5' src="/arrow_icon.svg"/></Link>
+      <div className='group h-[700px] flex flex-col items-center justify-end text-center pb-15 relative overflow-hidden'>
+        <img
+          src="/about_bg.png"
+          alt=""
+          className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 transform group-hover:scale-105"
+        />
+        <p className='text-neutral-50 z-2'>Sobre Mí</p>
+        <h3 className='text-neutral-50 text-[48px] font-display mx-0 my-4 leading-none z-2'>Andrés Marín</h3>
+        <Link href="/porsche_cup" className='text-primary font-body-light-italic flex gap-2 items-center z-2'>LEER MÁS <img className='h-5' src="/arrow_icon.svg"/></Link>
       </div>
-      <div className='h-[700px] flex flex-col items-center justify-end text-center pb-15 bg-[url(/services_bg.png)] bg-cover bg-center bg-no-repeat'>
-        <p className='text-neutral-50'>Ofrezco</p>
-        <h3 className='text-neutral-50 text-[48px] font-display mx-0 my-4 leading-none'>Servicios</h3>
-        <Link href="/porsche_cup" className='text-primary font-body-light-italic flex gap-2 items-center'>LEER MÁS <img className='h-5' src="/arrow_icon.svg"/></Link>
+      <div className='group h-[700px] flex flex-col items-center justify-end text-center pb-15 relative overflow-hidden'>
+        <img
+          src="/services_bg.png"
+          alt=""
+          className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 transform group-hover:scale-105"
+        />
+        <p className='text-neutral-50 z-2'>Ofrezco</p>
+        <h3 className='text-neutral-50 text-[48px] font-display mx-0 my-4 leading-none z-2'>Servicios</h3>
+        <Link href="/porsche_cup" className='text-primary font-body-light-italic flex gap-2 items-center z-2'>LEER MÁS <img className='h-5' src="/arrow_icon.svg"/></Link>
       </div>
-      <div className='h-[700px] flex flex-col items-center justify-end text-center pb-15 bg-[url(/socials_bg.png)] bg-cover bg-center bg-no-repeat'>
-        <p className='text-neutral-50'>Sígueme en</p>
-        <h3 className='text-neutral-50 text-[48px] font-display mx-0 my-4 leading-none'>Redes Sociales</h3>
-        <Link href="/porsche_cup" className='text-primary font-body-light-italic flex gap-2 items-center'>LEER MÁS <img className='h-5' src="/arrow_icon.svg"/></Link>
+      <div className='group h-[700px] flex flex-col items-center justify-end text-center pb-15 relative overflow-hidden'>
+        <img
+          src="/socials_bg.png"
+          alt=""
+          className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 transform group-hover:scale-105"
+        />
+        <p className='text-neutral-50 z-2'>Sígueme en</p>
+        <h3 className='text-neutral-50 text-[48px] font-display mx-0 my-4 leading-none z-2'>Redes Sociales</h3>
+        <Link href="/porsche_cup" className='text-primary font-body-light-italic flex gap-2 items-center z-2'>LEER MÁS <img className='h-5' src="/arrow_icon.svg"/></Link>
       </div>
-      <div className='h-[700px] flex flex-col items-center justify-end text-center pb-15 bg-[url(/sponsors_bg.png)] bg-cover bg-center bg-no-repeat'>
-        <p className='text-neutral-50'>Apóyame</p>
-        <h3 className='text-neutral-50 text-[48px] font-display mx-0 my-4 leading-none'>Sponsor Me</h3>
-        <Link href="/porsche_cup" className='text-primary font-body-light-italic flex gap-2 items-center'>LEER MÁS <img className='h-5' src="/arrow_icon.svg"/></Link>
+      <div className='group h-[700px] flex flex-col items-center justify-end text-center pb-15 relative overflow-hidden'>
+        <img
+          src="/sponsors_bg.png"
+          alt=""
+          className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 transform group-hover:scale-105"
+        />
+        <p className='text-neutral-50 z-2'>Apóyame</p>
+        <h3 className='text-neutral-50 text-[48px] font-display mx-0 my-4 leading-none z-2'>Sponsor Me</h3>
+        <Link href="/porsche_cup" className='text-primary font-body-light-italic flex gap-2 items-center z-2'>LEER MÁS <img className='h-5' src="/arrow_icon.svg"/></Link>
       </div>
+
     </div>
   )
 }
