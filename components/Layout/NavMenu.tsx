1aa3fb06da47db550c9c9aa906549ce65aff5cca
Matias Aranda <arandamatias849@gmail.com>
2025-05-20 01:26:57 -0300

    fixed navmenu width

--- components/Layout/NavMenu.tsx
+++ components/Layout/NavMenu.tsx
@@ -53,7 +53,7 @@ const NavMenu: React.FC<NavMenuProps> = ({ setShown, setLoading }) => {
   return (
     <div className='h-screen w-full flex fixed justify-end top-0 left-0 z-10'>
         <div ref={bgRef} onClick={closeModal} className='bg-[#0C0C1D]/90 absolute w-full h-full z-10'></div>
-        <div ref={modalRef} className='bg-background/50 w-[40%] backdrop-blur flex flex-col items-start justify-center gap-6 border-l border-l-primary p-10 z-10 relative'>
+        <div ref={modalRef} className='bg-background/50 min-w-[250px] w-[40vw] backdrop-blur flex flex-col items-start justify-center gap-6 border-l border-l-primary p-10 z-10 relative'>
             <div onClick={() => setShown(false)} className='flex flex-col items-start justify-center gap-6'>
                 <TransitionLink setLoading={setLoading} className='w-full' href="/">Inicio</TransitionLink>
                 <TransitionLink setLoading={setLoading} className='w-full' href="/about">Sobre Mí</TransitionLink>
