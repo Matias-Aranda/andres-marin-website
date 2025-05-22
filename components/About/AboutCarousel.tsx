d60e1e3ceba384451480f46bc18efb46b09a6667
Matias Aranda <arandamatias849@gmail.com>
2025-05-22 14:18:05 -0300

    improved responsiveness on about section

--- components/About/AboutCarousel.tsx
+++ components/About/AboutCarousel.tsx
@@ -19,21 +19,21 @@ const AboutCarousel = () => {
 
   return (
     <div className='h-screen flex flex-col items-center justify-center text-center bg-neutral-50 relative overflow-hidden'>
-        <div className={`flex items-center justify-center gap-49 transition-opacity duration-1000 absolute ${count == 0 ? "opacity-100" : "opacity-0"}`}>
-            <img src="/sign.svg" />
-            <div className='w-[40%] text-left flex flex-col gap-6'>
-                <h3 className='text-neutral-900 text-[64px] font-display mx-0 my-4 leading-none'>Sobre <span className='text-primary'>Mí</span></h3>
+        <div className={`flex flex-col lg:flex-row items-center justify-center gap-[10%] transition-opacity duration-1000 absolute ${count == 0 ? "opacity-100" : "opacity-0"}`}>
+            <img src="/sign.svg"  className='w-[80%] sm:w-[70%] md:w-[50%] lg:w-[30%]'/>
+            <div className='w-[90%] sm:w-[80%] lg:w-[40%] text-left flex flex-col gap-6'>
+                <h3 className='text-neutral-900 text-[48px] sm:text-[64px] font-display mx-0 my-0 mt-10 lg:my-4 leading-none'>Sobre <span className='text-primary'>Mí</span></h3>
                 <p>Compitiendo en una de las series más desafiantes de iRacing, la Porsche Cup (Fixed), busco consolidarme entre los mejores del mundo, compartiendo el proceso y la realidad detrás del Simracing competitivo.</p>
-                <div className='w-40 mt-15 border-b-1 border-primary'></div>
-                <p className='uppercase font-body-light tracking-[.8em]'>Simracing Coach</p>
+                <div className='w-40 mt-0 lg:mt-15 border-b-1 border-primary'></div>
+                <p className='uppercase font-body-light tracking-[.6em]  sm:tracking-[.8em] font-base'>Simracing Coach</p>
             </div>
         </div>
-        <div className={`flex items-center justify-center gap-20 transition-opacity duration-1000 absolute ${count == 1 ? "opacity-100" : "opacity-0 pointer-events-none"}`}>
-            <img src="/steering_wheel.png" />
-            <div className='w-[40%] text-left flex flex-col gap-6'>
-                <h3 className='text-neutral-900 text-[64px] font-display mx-0 my-4 leading-none'>Porsche <span className='text-primary'>Cup</span></h3>
+        <div className={`flex flex-col lg:flex-row items-center justify-center gap-10 xl:gap-20 transition-opacity duration-1000 absolute ${count == 1 ? "opacity-100" : "opacity-0 pointer-events-none"}`}>
+            <img src="/steering_wheel.png" className='w-[90%] sm:w-[80%] h-[400px] md:h-[500px] lg:h-[initial] object-cover lg:w-[35%] rounded-md' />
+            <div className='w-[90%] sm:w-[80%] lg:w-[40%] text-left flex flex-col gap-6'>
+                <h3 className='text-neutral-900 text-[48px] sm:text-[64px] font-display mx-0 my-0 lg:my-4 leading-none'>Porsche <span className='text-primary'>Cup</span></h3>
                 <p>Compitiendo en una de las series más desafiantes de iRacing, la Porsche Cup (Fixed), busco consolidarme entre los mejores del mundo, compartiendo el proceso y la realidad detrás del Simracing competitivo.</p>
-                <Link href="/porsche_cup" className='text-neutral-900 text-3xl font-body-light-italic flex gap-4 items-center mt-15 w-fit'>LEER MÁS <img src="/arrow_icon_black.svg"/></Link>
+                <Link href="/porsche_cup" className='text-neutral-900 text-2xl xl-:text-3xl font-body-light-italic flex gap-4 items-center lg:mt-15 w-fit'>LEER MÁS <img src="/arrow_icon_black.svg"/></Link>
             </div>
         </div>
     </div>
--- components/About/AboutStats.tsx
+++ components/About/AboutStats.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const AboutStats = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-5 py-20 text-white h-screen overflow-hidden'>
-        <h2 className='text-[96px] font-display'>Estadísticas</h2>
+    <div className='flex flex-col items-center justify-center gap-5 py-20 px-10 sm:px-20 text-white h-screen overflow-hidden'>
+        <h2 className='text-[36px] sm:text-[48px] md:text-[72px] lg:text-[96px] font-display'>Estadísticas</h2>
         <img src="/porsche_cup.png" className='w-[800px] mb-10'/>
-        <div className='flex items-center justify-center gap-10'>
-            <div className='flex flex-col items-center justify-center'>
-                <h3 className='text-[96px] font-display leading-[.9]'><span className='text-primary'>+</span>1000</h3>
-                <p>Horas de experiencia</p>
+        <div className='flex flex-col sm:flex-row items-center justify-center gap-10 w-full'>
+            <div className='flex flex-col self-start sm:self-center items-center justify-center'>
+                <h3 className='text-[72px] sm:text-[48px] md:text-[72px] lg:text-[96px] font-display leading-[.9]'><span className='text-primary'>+</span>1000</h3>
+                <p className='text-center'>Horas de experiencia</p>
             </div>
-            <div className='flex flex-col items-center justify-center'>
-                <h3 className='text-[96px] font-display leading-[.9]'><span className='text-primary'>+</span>1000</h3>
-                <p>Horas de experiencia</p>
+            <div className='flex flex-col self-end sm:self-center items-center justify-center'>
+                <h3 className='text-[72px] sm:text-[48px] md:text-[72px] lg:text-[96px] font-display leading-[.9]'><span className='text-primary'>+</span>1000</h3>
+                <p className='text-center'>Horas de experiencia</p>
             </div>
-            <div className='flex flex-col items-center justify-center'>
-                <h3 className='text-[96px] font-display leading-[.9]'><span className='text-primary'>+</span>1000</h3>
-                <p>Horas de experiencia</p>
+            <div className='flex flex-col self-start sm:self-center items-center justify-center'>
+                <h3 className='text-[72px] sm:text-[48px] md:text-[72px] lg:text-[96px] font-display leading-[.9]'><span className='text-primary'>+</span>1000</h3>
+                <p className='text-center'>Horas de experiencia</p>
             </div>
         </div>
     </div>
