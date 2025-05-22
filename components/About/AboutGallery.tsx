8630cd1c0ef641e019b193a9150827d0dc7f004b
Matias Aranda <arandamatias849@gmail.com>
2025-05-22 17:31:43 -0300

    Changed slider carousel provider and improved responsiveness on gallery section

--- components/About/AboutGallery.tsx
+++ components/About/AboutGallery.tsx
@@ -1,65 +1,49 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import Draggable from 'gsap/Draggable';
 import InertiaPlugin from 'gsap/InertiaPlugin';
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 gsap.registerPlugin(Draggable, InertiaPlugin);
 
 const DraggableSlider = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const contentRef = useRef<HTMLDivElement>(null);
-  const images = [
-    "/gallery/1.png",
-    "/gallery/2.png",
-    "/gallery/3.png",
-    "/gallery/4.png",
-    
-  ]
+
+  const [mode, setMode] = useState("snap");
 
   useEffect(() => {
-    if (!containerRef.current || !contentRef.current) return;
+    const handleResize = () => {
+      setMode(window.innerWidth < 768 ? "snap" : "free-snap");
+    }
 
-    const container = containerRef.current;
-    const content = contentRef.current;
+    // Set on mount
+    handleResize()
 
-    const containerWidth = container.offsetWidth;
-    const itemWidth = 1000 + 50; // item + gap
-    const totalItems = content.children.length;
-
-    const totalWidth = itemWidth * totalItems;
-
-    const centerOffset = (containerWidth - itemWidth) / 2;
-
-    const minX = -(itemWidth * totalItems - containerWidth + centerOffset);
-    const maxX = centerOffset;
-
-    Draggable.create(content, {
-      type: 'x',
-      inertia: true,
-      cursor: 'grab',
-      activeCursor: 'grabbing',
-      bounds: { minX, maxX },
-      snap: {
-        x: (endValue) => {
-          const index = Math.round((endValue - centerOffset) / itemWidth);
-          return index * itemWidth + centerOffset;
-        },
-      },
-    });
+    // Listen for resize
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
   }, []);
 
+  const [sliderRef] = useKeenSlider({
+    loop: false,
+    mode: mode,
+    slides: {
+      origin: "center",
+      perView: () => (window.innerWidth > 1600 ? 2 : window.innerWidth < 768 ? 1 : 1.5),
+      spacing: () => (window.innerWidth < 1280 ? 20 : 50),
+    },
+  })
+
   return (
-    <div ref={containerRef} className="overflow-hidden w-full mx-auto bg-white py-40 relative">
-      <h3 className='absolute font-body-light-italic uppercase top-30 left-105 text-[28px]'>Galería</h3>
-      <div ref={contentRef} className="flex gap-[50px] w-max p-4">
-        {images.map((image, i) => (
-          <img src={image}
-            key={i}
-            className="w-[1000px] h-[600px] bg-blue-500 text-white flex items-center justify-center shadow-md text-2xl object-cover"
-          />
-        ))}
+    <div className="overflow-hidden w-full mx-auto bg-white py-40 relative">
+      <h3 className='absolute font-body-light-italic uppercase top-30 left-[5%] md:left-[17%] text-[28px]'>Galería</h3>
+      <div ref={sliderRef} className="keen-slider my-10">
+        <img src="/gallery/1.png" className="keen-slider__slide h-[500px] xl:h-[600px] object-cover" />
+        <img src="/gallery/2.png" className="keen-slider__slide h-[500px] xl:h-[600px] object-cover" />
+        <img src="/gallery/3.png" className="keen-slider__slide h-[500px] xl:h-[600px] object-cover" />
+        <img src="/gallery/4.png" className="keen-slider__slide h-[500px] xl:h-[600px] object-cover" />
       </div>
     </div>
   );
