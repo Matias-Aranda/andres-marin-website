bddc12fe0d12736eecd629b8ab7bf132d3664115
Matias Aranda <arandamatias849@gmail.com>
2025-05-23 16:10:05 -0300

    improved responsiveness on services section

--- components/Services/ServicesGuide.tsx
+++ components/Services/ServicesGuide.tsx
@@ -3,10 +3,11 @@ import Link from 'next/link'
 
 const ServicesGuide = () => {
   return (
-    <div className='h-screen bg-[url(/guide_bg.png)] flex flex-col justify-center bg-cover bg-center bg-no-repeat p-40'>
-        <div className='flex flex-col items-start justify-center w-[50%]'>
-            <h1 className='text-neutral-50 text-[150px] font-display-condensed mx-0 my-4 leading-[0.9] uppercase'>Nuevo en el <span className='text-primary'>Simracing?</span></h1>
-            <p className='text-neutral-50 w-[450px] my-6'>Descarga mi Guía <span className='text-primary'>GRATUITA</span> para encontrar tu camino y orientar tu dinero de manera correcta.</p>
+    <div className='h-screen bg-[url(/guide_bg.png)] bg-right flex flex-col justify-center bg-cover lg:bg-center bg-no-repeat p-[8%] relative'>
+        <div className='absolute top-0 left-0 h-full w-full backdrop-brightness-40 xl:backdrop-brightness-100 z-1'></div>
+        <div className='flex flex-col items-start justify-center w-[70%] md:w-[650px] z-2'>
+            <h1 className='text-neutral-50 text-[72px] sm:text-[92px] md:text-[150px] font-display-condensed mx-0 my-4 leading-[0.9] uppercase'>Nuevo en el <span className='text-primary'>Simracing?</span></h1>
+            <p className='text-neutral-50 w-[60%] md:w-[450px] my-6'>Descarga mi Guía <span className='text-primary'>GRATUITA</span> para encontrar tu camino y orientar tu dinero de manera correcta.</p>
             <Link href="#" className='text-base text-background my-2 px-20 py-2.5 bg-primary rounded-md'>Descargar</Link>
         </div>
     </div>
--- components/Services/ServicesHero.tsx
+++ components/Services/ServicesHero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ServicesHero = () => {
   return (
     <div className='h-[90vh] flex flex-col items-center justify-end text-center pb-15 bg-[url(/services_bg.png)] bg-cover bg-center bg-no-repeat border-b border-primary'>
-        <h1 className='text-neutral-50 text-[128px] font-display m-0'>Servicios</h1>
+        <h1 className='text-neutral-50 text-[clamp(3.5rem,12vw,6rem)] sm:text-[72px] lg:text-[96px] xl:text-[128px] font-display mx-10 mb-10 leading-none'>Servicios</h1>
     </div>
   )
 }
