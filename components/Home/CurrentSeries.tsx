ec6017c01cda74d675292191087a7d2d7409a04f
Matias Aranda <arandamatias849@gmail.com>
2025-05-24 08:59:38 -0300

    changed hero bg video

--- components/Home/CurrentSeries.tsx
+++ components/Home/CurrentSeries.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CurrentSeries = () => {
   return (
-    <div className='h-screen flex flex-col items-center justify-center bg-radial from-slate-900 to-gray-950 px-10'>
+    <div className='mt-10 p-30 sm:h-screen flex flex-col items-center justify-center bg-radial from-slate-900 to-background to-70% px-10'>
         <h1 className='text-primary text-[48px] md:text-[72px] lg:text-[96px] font-display m-0 leading-none text-center'><span className='text-neutral-50'>Porsche</span> Cup</h1>
         <p className='text-neutral-50'>(Actualmente Compitiendo)</p>
         <img src="/porsche_cup.png" className='mt-10 md:mt-0'/>
--- components/Home/SeriesOverview.tsx
+++ components/Home/SeriesOverview.tsx
@@ -3,16 +3,19 @@ import React from 'react'
 
 const SeriesOverview = () => {
   return (
-    <div className='flex flex-col lg:flex-row items-center justify-center h-screen p-15 sm:p-30 overflow-hidden gap-6'>
+    <div className='flex flex-col lg:flex-row items-center justify-center pb-30 p-15 sm:px-30 overflow-hidden gap-6 bg-gradient-to-t from-slate-500/5 to-background to-70% px-10'>
         <div className='flex flex-col items-center lg:items-start'>
-            <h1 className='text-primary text-[48px] sm:text-[72px] 2xl:text-[96px] font-display mx-0 my-6 2xl:my-12 text-center lg:text-left leading-none'><span className='text-neutral-50'>Porsche</span> Cup</h1>
+            <h1 className='text-primary text-[48px] sm:text-[72px] 2xl:text-[96px] font-display text-center lg:text-left leading-none'><span className='text-neutral-50'>Porsche</span> Cup</h1>
             <img src="/porsche_cup_fixed_badge.png" className='w-[80%] lg:hidden'/>
-            <p className='text-neutral-50 leading-relaxed w-full 2xl:w-4/5 my-6 2xl:my-12'>
+            <p className='text-neutral-50 leading-relaxed w-full 2xl:w-4/5 my-6 '>
                 Compitiendo en una de las series más desafiantes de iRacing, la Porsche Cup (Fixed), 
                 busco consolidarme entre los mejores del mundo, compartiendo el proceso y 
                 la realidad detrás del Simracing competitivo.
             </p>
-            <Link href="/porsche_cup" className='text-primary text-3xl font-body-light-italic flex gap-4 items-center self-start'>LEER MÁS <img src="/arrow_icon.svg"/></Link>
+            <Link href="/porsche_cup" className='text-primary text-base sm:text-xl lg:text-3xl font-body-light-italic flex gap-2 lg:gap-4 items-center self-start'>
+              LEER MÁS 
+              <img src="/arrow_icon.svg" className=''/>
+            </Link>
         </div>
         <img src="/porsche_cup_fixed_badge.png" className='hidden lg:block lg:w-[50%]'/>
     </div>
